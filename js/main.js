98df8631cc541def046e5e6d84cea7f43a2d665f
Omri Shalev <omriknight9@gmail.com>
2020-02-17 15:33:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,15 @@ $(document).ready(function (event) {
 
     setTimeout(function() {
         if ($(window).width() > 765) {
+            // test(document.getElementById("moveBtn0"), $('#secondImg0'));
             dragElement(document.getElementById("moveBtn0"), $('#secondImg0'));
-            dragElement(document.getElementById("moveBtn1"), $('#secondImg1'));
-            dragElement(document.getElementById("moveBtn2"), $('#secondImg2'));
-            dragElement(document.getElementById("moveBtn3"), $('#secondImg3'));
-            dragElement(document.getElementById("moveBtn4"), $('#secondImg4'));
-            dragElement(document.getElementById("moveBtn5"), $('#secondImg5'));
-            dragElement(document.getElementById("moveBtn6"), $('#secondImg6'));
+            test(document.getElementById("moveBtn1"), $('#secondImg1'));
+            // dragElement(document.getElementById("moveBtn1"), $('#secondImg1'));
+            // dragElement(document.getElementById("moveBtn2"), $('#secondImg2'));
+            // dragElement(document.getElementById("moveBtn3"), $('#secondImg3'));
+            // dragElement(document.getElementById("moveBtn4"), $('#secondImg4'));
+            // dragElement(document.getElementById("moveBtn5"), $('#secondImg5'));
+            // dragElement(document.getElementById("moveBtn6"), $('#secondImg6'));
         } else {
             dragElementMobile(document.getElementById("moveBtn0"), $('#secondImg0'));
             dragElementMobile(document.getElementById("moveBtn1"), $('#secondImg1'));
@@ -34,6 +36,122 @@ $(document).ready(function (event) {
         scrollBtn();
     }
 });
+
+
+function test(item, img) {
+
+    let pos1 = 0, pos2 = 0;
+
+    let finalWidth = $(item).parent().parent().css('width');
+    let finalHeight = $(item).parent().parent().css('height');
+    let widthNum = finalWidth.replace('px', '') - 40;
+
+    var dragItem = item;
+    // var container = document.querySelector("#container");
+
+    var container = $(item).parent().parent().parent()[0];
+    console.log(container);
+    console.log(img);
+    var active = false;
+    var currentX;
+    var currentY;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    function dragStart(e) {
+        e = e || window.event;
+        e.preventDefault();
+      if (e.type === "touchstart") {
+        pos2 = e.touches[0].clientX;
+      } else {
+        pos2 = e.clientX;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+        console.log('------------------------------------');
+      initialX = currentX;
+      initialY = currentY;
+
+      $(item).css('cursor', 'grab');
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+        $(item).css('cursor', 'grabbing');
+        e = e || window.event;
+        e.preventDefault();
+        // pos1 = pos2 - e.clientX;
+        // pos2 = e.clientX;
+      
+        if (e.type === "touchmove") {
+            currentX = e.touches[0].clientX - initialX;
+            currentY = e.touches[0].clientY - initialY;
+            pos1 = pos2 - e.touches[0].clientX;
+            pos2 = e.touches[0].clientX;
+            item.style.left = (item.offsetLeft - pos1) + "px";
+            console.log('asd');
+            $(img).css('clip', "rect(0," + (item.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
+        } else {
+            currentX = e.clientX - initialX;
+            currentY = e.clientY - initialY;
+            pos1 = pos2 - e.clientX;
+            pos2 = e.clientX;
+            console.log(pos1);
+            console.log('item.offsetLeft: ' + item.offsetLeft);
+            console.log(item.offsetLeft - pos1);
+            item.style.left = (item.offsetLeft - pos1) + "px";
+            $(img).css('clip', "rect(0," + (item.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + 0 + "px, 0)";
+    }
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function buildImages() {
     
@@ -165,6 +283,11 @@ function dragElement(elmnt, img) {
         e.preventDefault();
         pos1 = pos2 - e.clientX;
         pos2 = e.clientX;
+
+        console.log(pos1);
+        console.log('elmnt.offsetLeft: ' + elmnt.offsetLeft);
+        console.log(elmnt.offsetLeft - pos1);
+        
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
         $(img).css('clip', "rect(0," + (elmnt.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
         if (elmnt.offsetLeft - pos1 < 0) {
