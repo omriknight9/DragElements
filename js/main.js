7976638c6fe8b4b1f5304cb822792f61fbb2c4a0
Omri Shalev <omriknight9@gmail.com>
2020-02-17 16:57:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,31 +6,13 @@ $(document).ready(function (event) {
     buildImages();
 
     setTimeout(function() {
-        test(document.getElementById("moveBtn0"), $('#secondImg0'));
-        test(document.getElementById("moveBtn1"), $('#secondImg1'));
-        test(document.getElementById("moveBtn2"), $('#secondImg2'));
-        test(document.getElementById("moveBtn3"), $('#secondImg3'));
-        test(document.getElementById("moveBtn4"), $('#secondImg4'));
-        test(document.getElementById("moveBtn5"), $('#secondImg5'));
-        test(document.getElementById("moveBtn6"), $('#secondImg6'));
-        // if ($(window).width() < 765) {
-        //     dragElement(document.getElementById("moveBtn0"), $('#secondImg0'));
-        //     dragElement(document.getElementById("moveBtn1"), $('#secondImg1'));
-        //     dragElement(document.getElementById("moveBtn2"), $('#secondImg2'));
-        //     dragElement(document.getElementById("moveBtn3"), $('#secondImg3'));
-        //     dragElement(document.getElementById("moveBtn4"), $('#secondImg4'));
-        //     dragElement(document.getElementById("moveBtn5"), $('#secondImg5'));
-        //     dragElement(document.getElementById("moveBtn6"), $('#secondImg6'));
-        // } else {
-        //     test(document.getElementById("moveBtn0"), $('#secondImg0'));
-        //     test(document.getElementById("moveBtn1"), $('#secondImg1'));
-        //     test(document.getElementById("moveBtn2"), $('#secondImg2'));
-        //     test(document.getElementById("moveBtn3"), $('#secondImg3'));
-        //     test(document.getElementById("moveBtn4"), $('#secondImg4'));
-        //     test(document.getElementById("moveBtn5"), $('#secondImg5'));
-        //     test(document.getElementById("moveBtn6"), $('#secondImg6'));
-        // }
-
+        dragElement(document.getElementById("moveBtn0"), $('#secondImg0'));
+        dragElement(document.getElementById("moveBtn1"), $('#secondImg1'));
+        dragElement(document.getElementById("moveBtn2"), $('#secondImg2'));
+        dragElement(document.getElementById("moveBtn3"), $('#secondImg3'));
+        dragElement(document.getElementById("moveBtn4"), $('#secondImg4'));
+        dragElement(document.getElementById("moveBtn5"), $('#secondImg5'));
+        dragElement(document.getElementById("moveBtn6"), $('#secondImg6'));
     }, 500)
 
     window.onbeforeunload = function () {
@@ -45,12 +27,12 @@ $(document).ready(function (event) {
 
     window.onclick = function () {
         console.log('asd');
-        $('#test').html($(window).scrollTop());
+        $('#test').html('asd' + $(window).scrollTop());
     }
 });
 
 
-function test(item, img) {
+function dragElement(item, img) {
 
     let pos1 = 0, pos2 = 0;
 
@@ -62,8 +44,8 @@ function test(item, img) {
     // var container = document.querySelector("#container");
 
     var container = $(item).parent().parent().parent()[0];
-    console.log(container);
-    console.log(img);
+    // var container = item;
+
     var active = false;
     var currentX;
     var currentY;
@@ -108,8 +90,6 @@ function test(item, img) {
         $(item).css('cursor', 'grabbing');
         e = e || window.event;
         e.preventDefault();
-        // pos1 = pos2 - e.clientX;
-        // pos2 = e.clientX;
       
         if (e.type === "touchmove") {
             currentX = e.touches[0].clientX - initialX;
@@ -117,7 +97,6 @@ function test(item, img) {
             pos1 = pos2 - e.touches[0].clientX;
             pos2 = e.touches[0].clientX;
             item.style.left = (item.offsetLeft - pos1) + "px";
-            console.log('asd');
             $(img).css('clip', "rect(0," + (item.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
             if (item.offsetLeft - pos1 > widthNum) {
                 item.style.left = widthNum + 'px';
@@ -134,9 +113,6 @@ function test(item, img) {
             currentY = e.clientY - initialY;
             pos1 = pos2 - e.clientX;
             pos2 = e.clientX;
-            console.log(pos1);
-            console.log('item.offsetLeft: ' + item.offsetLeft);
-            console.log(item.offsetLeft - pos1);
             item.style.left = (item.offsetLeft - pos1) + "px";
             $(img).css('clip', "rect(0," + (item.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
 
@@ -167,26 +143,6 @@ function test(item, img) {
 
 }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function buildImages() {
     
     for (let i = 0; i < imgArr.length; i++) {
@@ -221,161 +177,6 @@ function buildImages() {
         }).appendTo(overlay); 
     }
 }
-
-function dragElementMobile(elmnt, img) {
-    let pos1 = 0, pos2 = 0
-
-    let container = $(elmnt).parent().parent().parent()[0];
-    console.log(container);
-
-    // elmnt.addEventListener("touchstart", dragMouseDown, false);
-    // elmnt.addEventListener("touchend", closeDragElement, false);
-    // elmnt.addEventListener("touchmove", elementDrag, false);
-
-    elmnt.addEventListener("touchstart", dragMouseDown, false);
-    // elmnt.addEventListener("mousedown", dragMouseDown, false);
-
-
-    let finalWidth = $(elmnt).parent().parent().css('width');
-    let finalHeight = $(elmnt).parent().parent().css('height');
-    let widthNum = finalWidth.replace('px', '') - 40;
-
-    // elmnt.onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
-      
-        e = e || window.event;
-        e.preventDefault();
-        pos2 = e.clientX;
-
-        container.addEventListener("touchend", closeDragElement, false);
-        container.addEventListener("touchmove", elementDrag, false);
-        
-        // document.addEventListener("mouseup", closeDragElement, false);
-        // document.addEventListener("mousemove", elementDrag, false);
-
-        // document.onmouseup = closeDragElement;
-        // document.onmousemove = elementDrag;
-
-    }
-  
-    function elementDrag(e) {
-        $(elmnt).css('cursor', 'grabbing');
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos2 - e.clientX;
-        pos2 = e.clientX;
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        $(img).css('clip', "rect(0," + (elmnt.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
-        if (elmnt.offsetLeft - pos1 < 0) {
-            elmnt.style.left = 0 + 'px';
-            $(img).css('clip', "rect(0," + 0 + ',' + 0 + ", 0");
-        }
-
-      if (elmnt.offsetLeft - pos1 > widthNum) {
-        elmnt.style.left = widthNum + 'px';
-        $(img).css('clip', "rect(0," + finalWidth + ',' + finalHeight + ", 0");
-      }
-    }
-  
-    function closeDragElement() {
-        console.log('zxcxzc');
-        $(elmnt).css('cursor', 'grab');
-        // document.onmouseup = null;
-        // document.onmousemove = null;
-        // document.removeEventListener("mouseup", closeDragElement, false);
-        // document.removeEventListener("mousemove", elementDrag, false);
-
-        container.removeEventListener("touchend", closeDragElement);
-        container.removeEventListener("touchmove", elementDrag);
-
-    }
-}
-    
-function dragElement(elmnt, img) {
-    let pos1 = 0, pos2 = 0
-
-    let finalWidth = $(elmnt).parent().parent().css('width');
-    let finalHeight = $(elmnt).parent().parent().css('height');
-    let widthNum = finalWidth.replace('px', '') - 40;
-
-    elmnt.onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos2 = e.clientX;
-        
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-
-    }
-  
-    function elementDrag(e) {
-        $(elmnt).css('cursor', 'grabbing');
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos2 - e.clientX;
-        pos2 = e.clientX;
-
-        console.log(pos1);
-        console.log('elmnt.offsetLeft: ' + elmnt.offsetLeft);
-        console.log(elmnt.offsetLeft - pos1);
-        
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        $(img).css('clip', "rect(0," + (elmnt.offsetLeft - pos1 + 20) + 'px' + ',' + finalHeight + ", 0");
-        if (elmnt.offsetLeft - pos1 < 0) {
-            elmnt.style.left = 0 + 'px';
-            $(img).css('clip', "rect(0," + 0 + ',' + 0 + ", 0");
-        }
-
-      if (elmnt.offsetLeft - pos1 > widthNum) {
-        elmnt.style.left = widthNum + 'px';
-        $(img).css('clip', "rect(0," + finalWidth + ',' + finalHeight + ", 0");
-      }
-    }
-  
-    function closeDragElement() {
-        $(elmnt).css('cursor', 'grab');
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
-
-function simulateMouseEvent (event, simulatedType) {
-
-    // Ignore multi-touch events
-    if (event.originalEvent.touches.length > 1) {
-      return;
-    }
-
-    event.preventDefault();
-
-    var touch = event.originalEvent.changedTouches[0],
-        simulatedEvent = document.createEvent('MouseEvents');
-    
-    // Initialize the simulated mouse event using the touch event's coordinates
-    simulatedEvent.initMouseEvent(
-      simulatedType,    // type
-      true,             // bubbles                    
-      true,             // cancelable                 
-      window,           // view                       
-      1,                // detail                     
-      touch.screenX,    // screenX                    
-      touch.screenY,    // screenY                    
-      touch.clientX,    // clientX                    
-      touch.clientY,    // clientY                    
-      false,            // ctrlKey                    
-      false,            // altKey                     
-      false,            // shiftKey                   
-      false,            // metaKey                    
-      0,                // button                     
-      null              // relatedTarget              
-    );
-
-    // Dispatch the simulated event to the target element
-    event.target.dispatchEvent(simulatedEvent);
-  }
 
 function goToTop() {
     $('html,body').animate({ scrollTop: 0 }, 'slow');
